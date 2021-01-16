779e1e43e54a792681539c87def5d3901aca6625
diekaltesonne <diekaltesonne2000@mail.ru>
2021-01-16 13:53:52 +0300

    edit#3 task minion-game done

--- PYTHON/the-minion-game.py
+++ PYTHON/the-minion-game.py
@@ -1,35 +1,29 @@
 # https://www.hackerrank.com/challenges/the-minion-game/problem
 
-def minion_game(word):
-    dictionary = {"A","E","I","O","U"}
-    stut = {}
-    kev = {}
-    a = stuart_algo(word,dictionary)
-    b = kevin_algo(word,dictionary)
-
-    if(a>b):
-        return "Stuart {}".format(a)
-    if(a<b):
-        return "Kevin {}".format(b)
-    return "Something goes wrong"
+def sub_algo_2(string, sub):
+    count = start = 0
+    while True:
+        start = string.find(sub, start) + 1
+        if start > 0:
+            count+=1
+        else:
+            return count
 
 def sub_algo(word,stut,substr):
-    stut[substr]=word.count(substr)
-    new_substr = substr+word[word.find(substr)+len(substr)]
-    if  word.find(substr)+len(substr)<(len(word)-1):
+    stut[substr]=sub_algo_2(word, substr)
+    print(stut[substr])
+    if  word.find(substr)+len(substr)>(len(word)-1):
+        return stut
+    else:
         new_substr = substr+word[word.find(substr)+len(substr)]
         return sub_algo(word,stut,new_substr)
-    else:
-        return stut
+       
 
 def stuart_algo(word,dictionary):
     stut = {}
     for letter in word:
         if letter not in dictionary:
-            if letter in stut:
-                stut[letter] = stut[letter]  + 1
-            else:
-                stut[letter] = 1
+            if letter not in stut:
                 sub_algo(word,stut,letter)
     return sum(list(stut.values()))
     
@@ -38,13 +32,39 @@ def kevin_algo(word,dictionary):
     kevin = {}
     for letter in word:
         if letter in dictionary:
-            if letter in kevin:
-                kevin[letter] = kevin[letter]  + 1
-            else:
-                kevin[letter] = 1
-                sub_algo(word,kevin,letter)
+            sub_algo(word,kevin,letter)
     return sum(list(kevin.values()))
+
+def minion_game_0(word):
+    dictionary = {"A","E","I","O","U"}
+    stut = {}
+    kev = {}
+    a = stuart_algo(word,dictionary)
+    print("play")
+    b = kevin_algo(word,dictionary)
+    print("Stuart",a)
+    print("Kevin",b)
+    if(a>b):
+        print("Stuart",a)
+    if(a<b):
+        print("Kevin",b)
+
+def minion_game(string):
+    vowel =['A','E','I','O','U']
+    S=0
+    K=0
+    for i in range(len(string)):
+        if string[i] in vowel:
+            K+= len(string)-i
+        else:
+            S+=len(string)-i
+    if S>K:
+        print("Stuart"+" "+ "%d" % S)
+    elif K>S:
+        print("Kevin"+" "+'%d' % K)
+    else:
+        print("Draw")
 
 if __name__ == '__main__':
     s = input()
-    print(minion_game(s))
+    minion_game(s)
