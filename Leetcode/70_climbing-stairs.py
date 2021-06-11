1e49568390e1f3bc7a359247643cdef204e92587
Ilya Trifonov <ilya.trifonov@zyfra.com>
2021-06-11 12:03:05 +0300

    11/06/21 u

---
+++ Leetcode/70_climbing-stairs.py
@@ -0,0 +1,17 @@
+from typing import List
+# - [How to Convert a Python String to int](https://realpython.com/convert-python-string-to-int/)
+
+class Solution:
+    def climbStairs(self, n: int) -> int:
+        45
+        pass
+
+def main():
+    a = Solution()
+    print(a.climbStairs(n = 2))
+    print(a.climbStairs(n = 3))
+
+
+
+if __name__ == "__main__":
+    main()
---
+++ Leetcode/88_merge-sorted-array.py
@@ -0,0 +1,19 @@
+from typing import List
+# - [How to Convert a Python String to int](https://realpython.com/convert-python-string-to-int/)
+
+class Solution:
+    def merge(self, nums1: List[int], m: int, nums2: List[int], n: int) -> None:
+        """
+        Do not return anything, modify nums1 in-place instead.
+        """
+        
+
+def main():
+    a = Solution()
+    print(a.merge(n = 2))
+    print(a.merge(n = 3))
+
+
+
+if __name__ == "__main__":
+    main()
